23cc363d8068002172fab389110f341a3471852f
Albina <albine89@mail.ru>
2022-10-05 00:38:47 +0300

    Task04 correct

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -19,4 +19,4 @@ if (number1 > max) max = number1;
 if (number2 > max) max = number2;
 if (number3 > max) max = number3;
 
-Console.WriteLine($"max = {max}");
+Console.WriteLine($"{number1}, {number2}, {number3} -> max = {max}");
